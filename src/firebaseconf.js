7cd0233ab751f9de8bf71179d244ebd6c74f0f53
BryanOrdonez23 <bryan.j.ordonez@unl.edu.ec>
2022-08-25 20:57:48 -0500

    actualizacion 25/08

--- src/firebaseconf.js
+++ src/firebaseconf.js
@@ -7,13 +7,22 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyCy9D8d6AfkmMr-TyzCaviEHWHVz4fi3Hc",
   authDomain: "login-acsolves.firebaseapp.com",
   projectId: "login-acsolves",
   storageBucket: "login-acsolves.appspot.com",
   messagingSenderId: "442943350064",
   appId: "1:442943350064:web:432c7e8afd34827a2294f5"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-vFdJkJtVXQZ_HS5Oxss0F48sDq6n39U",
+  authDomain: "parqueadero-8ff72.firebaseapp.com",
+  projectId: "parqueadero-8ff72",
+  storageBucket: "parqueadero-8ff72.appspot.com",
+  messagingSenderId: "995394008356",
+  appId: "1:995394008356:web:52b82c7cebabc1a31c6fb1"
 };
 
 // Initialize Firebase
